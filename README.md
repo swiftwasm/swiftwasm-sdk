906c4378e9ebde91e500c1a30860045d76954c61
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-04-21 09:38:27 -0700

    Add Azure Pipelines build scripts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Build scripts for SwiftWasm.
+
+[![Build Status](https://dev.azure.com/swiftwasm/swiftwasm-sdk/_apis/build/status/swiftwasm.swiftwasm-sdk?branchName=master)](https://dev.azure.com/swiftwasm/swiftwasm-sdk/_build/latest?definitionId=3&branchName=master)
---
+++ ci-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+sourcedir="$PWD"
+# install dependencies
+sudo apt update
+sudo apt install git cmake ninja-build clang python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev systemtap-sdt-dev tzdata rsync
+# install the WASI sdk
+wget -O wasisdk.deb "https://github.com/swiftwasm/wasi-sdk/releases/download/20190421.6/wasi-sdk_3.19gefb17cb478f9.m_amd64.deb"
+sudo dpkg -i wasisdk.deb
+# download ICU
+wget -O icu.tar.xz "https://github.com/swiftwasm/icu4c-wasi/releases/download/20190421.3/icu4c-wasi.tar.xz"
+tar xf icu.tar.xz
+# start build
+cd swift
+utils/build-script --release --wasm \
+	--llvm-targets-to-build "X86;WebAssembly" \
+	--llvm-max-parallel-lto-link-jobs 1 --swift-tools-max-parallel-lto-link-jobs 1 \
+	--wasm-wasi-sdk "/opt/wasi-sdk" \
+	--wasm-icu-uc "todo" \
+	--wasm-icu-uc-include "$sourcedir/icu_out/include" \
+	--wasm-icu-i18n "todo" \
+	--wasm-icu-i18n-include "todo" \
+	--wasm-icu-data "todo" \
+	--build-swift-static-stdlib \
+	--install-swift \
+	--install-prefix="/opt/swiftwasm-sdk" \
+	--install-destdir="$sourcedir/install" \
+	--installable-package="$sourcedir/swiftwasm.tar.gz"
+# copy the result
+cp "$sourcedir/swiftwasm.tar.gz" "$BUILD_ARTIFACTSTAGINGDIRECTORY/"
+
